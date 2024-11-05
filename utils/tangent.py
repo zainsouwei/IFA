3a5d16cdc43459c52ddb09788a58bc3b288e4595
zainsou <zainsou@umich.edu>
2024-11-05 19:12:14 +0100

    more deconfounding

--- utils/tangent.py
+++ utils/tangent.py
@@ -6,6 +6,7 @@ import sys
 sys.path.append('/utils')
 
 from classification import linear_classifier
+from regression import deconfound
 
 def tangent_transform(train, test=None, metric="riemann"):
     if test is not None:
@@ -21,8 +22,11 @@ def tangent_transform(train, test=None, metric="riemann"):
         train = tangent_space(train, Frechet_Mean, metric=metric)
         return train, Frechet_Mean
 
-def tangent_classification(covs_train, y_train, covs_test, y_test, clf_str="SVM (C=1)", z_score=2, metric="riemann"):
+def tangent_classification(covs_train, y_train, covs_test, y_test, clf_str="SVM (C=1)", z_score=2, metric="riemann", deconf=False, con_confounder_train=None, cat_confounder_train=None, con_confounder_test=None, cat_confounder_test=None):
     X_train, X_test, _ = tangent_transform(covs_train, covs_test, metric=metric)
+    if deconf:
+        X_train, X_test = deconfound(X_train, con_confounder_train, cat_confounder_train, X_test=X_test, con_confounder_test=con_confounder_test, cat_confounder_test=cat_confounder_test, age_var="Age_in_Yrs", sex_var="Gender")
+
     summary = linear_classifier(X_train, y_train, X_test, y_test, clf_str=clf_str, z_score=z_score)
 
     return summary
