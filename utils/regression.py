5e1ccff990126e7c6949cf284411647db127810b
zainsou <zainsou@umich.edu>
2024-11-07 18:21:50 +0100

    evaluation

--- utils/regression.py
+++ utils/regression.py
@@ -15,7 +15,7 @@ def deconfound(X_train, con_confounder_train, cat_confounder_train, X_test=None,
     encoder = OneHotEncoder(drop='first', sparse_output=False,handle_unknown='ignore')  # drop='first' to avoid multicollinearity
     cat_confounder_train_encoded = encoder.fit_transform(cat_confounder_train)
     print(cat_confounder_train_encoded.shape)
-    print(list(encoder.categories_[0]))
+    print(list(encoder.categories_))
     age_index = continuous_confounders.index(age_var)
     sex_index = categorical_confounders.index(sex_var)
 
